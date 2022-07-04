4a73543711c5e7f0597bb1f2c98e12068a296b1e
Alejandro Cortez <102841726+Muzan67@users.noreply.github.com>
2022-07-03 20:12:34 -0400

    updating js

---
+++ Develop/server.js
@@ -0,0 +1,53 @@
+// Import express package
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const util = require('./db/db/db.json');
+
+// Helper method for generating unique ids
+const uuid = require('./db/helpers/uuid');
+
+// Our Server
+const PORT = process.env.PORT || 3001;
+// Initialize our app variable by setting it to the value of express()
+const app = express();
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+// GET request for note || API Route
+app.get('/api/notes', (req, res) => {
+    const dataNotes = fs.readFileSync(path.join(__dirname, './db/db.json'), 'utf8');
+    const parseNotes = JSON.parse(dataNotes);  
+    res.json(parseNotes); 
+  });
+
+// POST request for note || API Route
+app.post('/api/notes', (req, res) => {
+    const dataNotes = fs.readFileSync(__dirname, './db/db.json', 'UTF-8');
+    const parseNotes = JSON.parse(dataNotes); 
+    req.body.id = uuid()
+    parseNotes.push(req.body);
+        
+    fs.writeFileSync(path.join(__dirname, './db/db.json'), JSON.stringify(parseNotes), 'UTF-8');
+    res.json("Note Added Successfully!");
+});
+
+// Add a static route for index.html
+
+app.get('/notes', (req, res) => {
+  // `res.sendFile` is Express' way of sending a file
+    // `__dirname` is a variable that always returns the directory that your server is running in
+    res.sendFile(path.join(__dirname, './public.notes.html'));
+  });
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public.index.html'));
+  });
+
+// Listening Port
+app.listen(PORT, function () {
+  console.log(`App listening on PORT: 🚀` + PORT);
+});
